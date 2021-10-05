df13663cd220edfaf61813f29e793562502e1101
vishalrajapure <vishalrajapure@gmail.com>
2021-10-05 18:30:18 +0530

    Adding product model class

--- src/com/promoengine/model/Product.java
+++ src/com/promoengine/model/Product.java
@@ -7,24 +7,29 @@ public class Product {
     public Product() {
     }
 
-    public Product(String productId, Double productPrice) {
+    public Product(String productId) {
         this.productId = productId;
-        this.productPrice = productPrice;
+        switch (productId){
+            case "A":
+                this.productPrice = 50.00;
+                break;
+            case "B":
+                this.productPrice = 30.00;
+                break;
+            case "C":
+                this.productPrice = 20.00;
+                break;
+            case "D":
+                this.productPrice = 15.00;
+                break;
+        }
     }
 
     public String getProductId() {
         return productId;
     }
 
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
     public Double getProductPrice() {
         return productPrice;
-    }
-
-    public void setProductPrice(Double productPrice) {
-        this.productPrice = productPrice;
     }
 }
